f9761df7d57530ded935fdb3490da0a72242c962
Samiul Hossain Fahim <samiul.fahimm@gmail.com>
2022-01-08 00:25:56 +0600

    webView App V1

---
+++ app/src/main/java/com/example/testwebview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.testwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import javax.security.auth.callback.Callback;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        web = findViewById(R.id.webView);
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        web.setWebViewClient(new Callback());
+        web.loadUrl("https://www.youtube.com/");
+
+    }
+    private class Callback extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
+    }
+}
